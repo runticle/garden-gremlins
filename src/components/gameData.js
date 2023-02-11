cca95eee15910ab58d111972afcc553d9771952a
Alfie Freeman <runticle@gmail.com>
2023-02-11 21:23:39 +0000

    settings object in gamedata. has default, min and max keys. needs some thinking on how to display intuitively in settings

--- src/components/gameData.js
+++ src/components/gameData.js
@@ -9,13 +9,55 @@ const GAME_DATA = {
         GUN_HEIGHT: 75
     },
 
+    // Changable in Menu
+    // Sets difficulty
+
+    // which direction is easier / harder? 
+    // can i normalise this and use 1-10 with a transform function?
+    DEFAULT_SETTINGS: {
+        GAME_PULSE: {
+            default: 8,
+            max: 10,
+            min: 0,
+            step: 1,
+
+        },
+        BULLET_SIZE: {
+            default: 4,
+            max: 10,
+            min: 0,
+        },
+        SHIT_SPEED: {
+            default: 4,
+            max: 10,
+            min: 0,
+        },
+        SHIT_SIZE: {
+            default: 12,
+            max: 10,
+            min: 0,
+        },
+        SHIT_ODDS: {
+            default: 0.005,
+            max: 10,
+            min: 0,
+        },
+        BIRD_SPEED_MULTIPLIER: {
+            default: 1,
+            max: 10,
+            min: 0,
+        },
+        BIRD_SIZE_MULTIPLIER: {
+            default: 1,
+            max: 10,
+            min: 0,
+        },
+    },
+
+
     // GAME DATA
-    BULLET_SIZE: 4,
-    GAME_PULSE: 8, // ms
     BIRD_HEIGHT: 40,
     BIRD_WIDTH: 40, // px. Only green bird. Tricky in future.
-    SHIT_SPEED: 4,
-    SHIT_SIZE: 12,
     // SHIT_ODDS: 0.005,
     MAX_GUN_POSITION: 300,
 
