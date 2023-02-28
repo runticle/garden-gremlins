4dafbbf33c260f80129d44fc383de3ba9eed2339
Alfie Freeman <runticle@gmail.com>
2023-02-28 21:22:05 +0000

    quick styles on settings just to release mvp

--- src/components/Menu/Settings.js
+++ src/components/Menu/Settings.js
@@ -11,13 +11,38 @@ const Editor = styled.div`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-
-
+    
+    
     .toggles {
         display: flex;
         flex-direction: row;   
+        align-items: center;
     }
 
+`
+
+const Bar = styled.div`
+    /* ${props => props.flip ? 'scaleX(-1)' : 'null'}; */
+
+    width: 300px;
+    opacity: 0.9;
+    background:#fce8a4;
+    height: 50px;
+    border-radius: 5px;
+    margin-right: 10px;
+
+    position: relative;
+
+    &:before{
+        content: '';
+        right: ${props => (100 - props.value * 10) + '%'};
+        /* right: 50px; */
+        position: absolute;
+        height: 100%;
+        left: 0;
+        background: linear-gradient(to right, #eab70e, brown);
+        transition: all 1s ease;
+    }
 `
 
 
@@ -34,9 +59,10 @@ export default function Settings({ difficulty = {}, updateDifficulty, onSave, sh
                 </h2>
                 <div className="editors">
                     {Object.keys(GAME_DATA.DEFAULT_SETTINGS).map((key, index) => {
-                        return <Editor max={20} key={index} current={13}>
-                            <span>{key.replace('_', ' ')}:{difficulty[key]}</span>
+                        return <Editor key={index}>
+                            <span>{key.replace('_', ' ')}</span>
                             <div className="toggles">
+                                <Bar value={difficulty[key]} />
                                 <button onClick={() => updateDifficulty(prev => ({
                                     ...prev,
                                     [key]: Math.max(1, difficulty[key] - 1)
@@ -53,7 +79,7 @@ export default function Settings({ difficulty = {}, updateDifficulty, onSave, sh
                         </Editor>
                     })}
                 </div>
-                <div className="presets">
+                <p className="presets">
                     <button onClick={() => updateDifficulty(GAME_DATA.DIFFICULTY_PRESETS.EASY)}>
                         Easy
                     </button>
@@ -63,10 +89,12 @@ export default function Settings({ difficulty = {}, updateDifficulty, onSave, sh
                     <button onClick={() => updateDifficulty(GAME_DATA.DIFFICULTY_PRESETS.HMM)}>
                         hmmm
                     </button>
-                </div>
-                <button onClick={onSave}>
-                    Save & Close
-                </button>
+                </p>
+                <p>
+                    <button onClick={onSave}>
+                        Save & Close
+                    </button>
+                </p>
             </InnerGameBox>
         </GameBox >
     )
